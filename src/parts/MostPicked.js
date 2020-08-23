fa070c684ece923021462078a2d1d455d4c23955
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-08-23 23:10:03 +0700

    Finished section categories

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,7 +4,7 @@ import Button from 'elements/Button'
 export default function MostPicked(props) {
   return (
     <section className="container" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
+      <h4 className="mb-3 font-weight-light">Most Picked</h4>
       <div className="container-grid">
         {props.data.map((item, index) => {
           return (
@@ -27,7 +27,7 @@ export default function MostPicked(props) {
                 <div className="meta-wrapper">
                   <Button
                     type="link"
-                    className="streched-link d-block text-white"
+                    className="stretched-link d-block text-white"
                     href={`/properties/${item._id}`}
                   >
                     <h5>{item.name}</h5>
