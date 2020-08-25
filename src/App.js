ebe4a2ee727c1654aaf54abcd302ea068532c961
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-08-25 14:04:51 +0700

    Adding Input Type Part

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'assets/scss/style.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingPage from 'pages/LandingPage';
+import Example from 'pages/Example';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Route path="/" component={LandingPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
