1eed9484f76293e18c1822c63fca011d63124903
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-08-28 22:41:25 +0700

    Checkout Header Logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LandingPage from 'pages/LandingPage';
 import DetailPage from 'pages/DetailPage';
 import Example from 'pages/Example';
+import Checkout from 'pages/Checkout';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailPage}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
         <Route path="/example" component={Example}></Route>
       </Router>
     </div>
